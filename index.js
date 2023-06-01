f1d367b047be4e2fba1073535fae3697cbd57312
Abasol <ramzeeesss666@gmail.com>
2023-06-01 23:46:14 +0800

    update

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+const test = document.querySelectorAll(".inline div");
+const screenRes = document.getElementById("result");
+const additions = document.getElementById("adds");
+let result = false;
+
+for (let i = 0; i < test.length; i++) {
+  test[i].addEventListener("click", () => {});
+}
